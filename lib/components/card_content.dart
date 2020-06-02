bc59df2ffa5e8bf90804d18736a292ee96e3067c
deepakDOTexe <iit2018116@iiita.ac.in>
2020-06-02 13:43:51 +0530

    update UI of results_page.dart

--- lib/components/card_content.dart
+++ lib/components/card_content.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'constants.dart';
+import '../constants.dart';
 
 class CardContentWidget extends StatelessWidget {
   CardContentWidget({ this.icon, this.text});
